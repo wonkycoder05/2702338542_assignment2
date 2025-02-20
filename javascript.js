0f073fbd2d5144e7ef81f6a9fbce9d889ea580b7
magdalena aretha <144786416+wonkycoder05@users.noreply.github.com>
2025-02-20 16:29:51 +0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -5,3 +5,31 @@ function myFunction(){
 function click(){
     window.alert("don't forget to drink water!");
 }
+
+let stars = 
+    document.getElementsByClassName("star");
+let output = 
+    document.getElementById("output");
+ 
+// Funtion to update rating
+function star(n) {
+    remove();
+    for (let i = 0; i < n; i++) {
+        if (n == 1) cls = "one";
+        else if (n == 2) cls = "two";
+        else if (n == 3) cls = "three";
+        else if (n == 4) cls = "four";
+        else if (n == 5) cls = "five";
+        stars[i].className = "star " + cls;
+    }
+    output.innerText = "Rating is: " + n + "/5";
+}
+ 
+// To remove the pre-applied styling
+function remove() {
+    let i = 0;
+    while (i < 5) {
+        stars[i].className = "star";
+        i++;
+    }
+}
